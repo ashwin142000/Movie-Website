b491b4bb04d52ff3d754f5220c2871b8bacefdc2
Ashwin Pottekkat <ashwinanilkumar14@gmail.com>
2024-07-05 14:28:31 +0530

    added auth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import { AuthContextProvider } from './context/AuthContext';
 
 function App() {
   return (
-    <div>
+    <AuthContextProvider>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/' element={<Home/>} />
       </Routes>
-    </div>
+    </AuthContextProvider>
   );
 }
 
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,10 +1,34 @@
 import { useContext, createContext, useEffect, useState } from "react";
-
+import {auth} from '../firebase';
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged} from 'firebase/auth';
+ 
 const AuthContext = createContext();
 
 export function AuthContextProvider({children}){
+    const [user,setUser]=useState({});
+    // const [password,setPassword]=useState({});
+
+    function signUp(user,password){
+        return createUserWithEmailAndPassword(auth, user, password);
+    }
+
+    function loginIn(user,password){
+        return signInWithEmailAndPassword(auth, user, password);
+    }
+
+    function logOut(){
+        return signOut(auth);
+    }
+
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth, (currentUser)=>{
+            setUser(currentUser);
+        });
+        return unsubscribe();
+    })
+
     return (
-        <AuthContext.Provider>
+        <AuthContext.Provider value={{signUp, loginIn, logOut, user}}>
             {children}
         </AuthContext.Provider>
 
